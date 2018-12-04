68eac9ea010ce28389d81fc2d4141b9a0b1c4a08
Dan Royer <dan@marginallyclever.com>
2018-12-04 13:02:14 -0800

    added motors 3 and 4

--- board_sanguinolulu.h
+++ board_sanguinolulu.h
@@ -8,19 +8,29 @@
 
 
 #if MOTHERBOARD == BOARD_SANGUINOLULU 
-#define MAX_MOTORS                 (2)
-
+#define MAX_MOTORS                 (4)
+//x
 #define MOTOR_0_DIR_PIN           (21)
 #define MOTOR_0_STEP_PIN          (15)
 #define MOTOR_0_ENABLE_PIN        (14)
 #define MOTOR_0_LIMIT_SWITCH_PIN  (18)
-
+//y
 #define MOTOR_1_DIR_PIN           (23)
 #define MOTOR_1_STEP_PIN          (22)
 #define MOTOR_1_ENABLE_PIN        (14)
 #define MOTOR_1_LIMIT_SWITCH_PIN  (19)
-
-// TODO: if ZARPLOTTER & SANGUINOLULU throw a compile error, not enough motors.
+//z
+#define MOTOR_2_DIR_PIN           (2)
+#define MOTOR_2_STEP_PIN          (3)
+#define MOTOR_2_ENABLE_PIN        (14)
+#define MOTOR_2_LIMIT_SWITCH_PIN  (20)
+//e
+#define MOTOR_3_DIR_PIN           (0)
+#define MOTOR_3_STEP_PIN          (1)
+#define MOTOR_3_ENABLE_PIN        (14)
+// the sanguinolulu doesn't have a limit switch pin for E.
+// I've assigned it to pin 24, the e-thermistor pin.  I have no idea if this will work.
+#define MOTOR_3_LIMIT_SWITCH_PIN  (24)
 
 #define MAX_BOARD_SERVOS           (1)
 #define SERVO0_PIN                (12)
@@ -28,4 +38,3 @@
 
 
 #endif  // BOARD_RAMPS_H
-
