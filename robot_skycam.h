0e6d6a9e6362513d5d4a6f0e7bde084387cc25e1
Dan Royer <dan@marginallyclever.com>
2019-06-17 13:36:34 -0700

    update default speeds

--- robot_skycam.h
+++ robot_skycam.h
@@ -16,18 +16,20 @@
 #define NUM_SERVOS           (0)
 #define NUM_TOOLS            (1)
 
-#define MAX_FEEDRATE         (15000.0)  // depends on timer interrupt & hardware
-#define MIN_FEEDRATE         (100)
-#define MAX_JERK             (15.0)
-#define DEFAULT_FEEDRATE     (10000.0)
-#define DEFAULT_ACCELERATION (3500)
+#define MAX_FEEDRATE         (100.0)  // depends on timer interrupt & hardware
+#define MIN_FEEDRATE         (0.0)
+#define DEFAULT_FEEDRATE     (90.0)
+
+#define MAX_ACCELERATION     (500.0)
+#define MIN_ACCELERATION     (0.0)
+#define DEFAULT_ACCELERATION (180.0)
+
+#define MAX_JERK             (10.0)
+#define MAX_Z_JERK           (0.3)
 
 #define SKYCAM_MOTOR_SIZE   (4.5f)
 #define SKYCAM_PLOTTER_SIZE (6.0f)
 #define SKYCAM_COMPENSATION (SKYCAM_PLOTTER_SIZE/2.0f + SKYCAM_MOTOR_SIZE)
-
-#define MAX_ACCELERATION     (5000)
-#define MIN_ACCELERATION     (100)
 
 #define SUBDIVIDE_LINES
 #define SEGMENT_PER_CM_LINE  (2)  // lines are split into segments.  How long are the segments?
@@ -41,4 +43,3 @@
 
 
 #endif  // #ifndef ROBOT_SKYCAM_H
-
