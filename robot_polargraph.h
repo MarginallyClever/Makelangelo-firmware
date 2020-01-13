362b7a15f54f45209fe9949fdb77f0fc401101da
Dan Royer <dan@marginallyclever.com>
2020-01-13 14:07:46 -0800

    2020 m5 settings

--- robot_polargraph.h
+++ robot_polargraph.h
@@ -22,8 +22,8 @@
 #define MACHINE_HARDWARE_VERSION   MAKELANGELO_5
 
 // choose one of the following
-//#define NORMAL_MOTOR_STEPS   200  // 1.8 degrees per step
-#define NORMAL_MOTOR_STEPS   400  // 0.9 degrees per step
+#define NORMAL_MOTOR_STEPS   200  // 1.8 degrees per step
+//#define NORMAL_MOTOR_STEPS   400  // 0.9 degrees per step
 /// -------------- change here ----------------
 
 
@@ -43,7 +43,7 @@
 #define NUM_SERVOS           (1)
 #define NUM_TOOLS            (1)
 
-#define MAX_FEEDRATE         (500.0)  // depends on timer interrupt & hardware
+#define MAX_FEEDRATE         (200.0)  // depends on timer interrupt & hardware
 #define MIN_FEEDRATE         (0.0)
 
 #define MAX_FEEDRATE_Z       (2000.0)  // depends on timer interrupt & hardware
@@ -52,13 +52,14 @@
 #define MIN_ACCELERATION     (0.0)
 
 #if NORMAL_MOTOR_STEPS == 200
-#define DEFAULT_FEEDRATE     (180.0)
-#define DEFAULT_ACCELERATION (300.0)
+#define DEFAULT_FEEDRATE     (65.0)
+#define MAX_FEEDRATE         (65.0)
+#define DEFAULT_ACCELERATION (150.0)
 #define DEGREES_PER_STEP     (1.8)
 #endif
 #if NORMAL_MOTOR_STEPS == 400
 #define DEFAULT_FEEDRATE     (100.0)
-#define DEFAULT_ACCELERATION (300.0)
+#define DEFAULT_ACCELERATION (150.0)
 #define DEGREES_PER_STEP     (0.9)
 #endif
 
@@ -69,24 +70,24 @@
 #define DYNAMIC_ACCELERATION
 
 #if MACHINE_HARDWARE_VERSION == MAKELANGELO_3
-#define MAX_SEGMENTS         (16)  // has LCD, needs more ram.
+#define MAX_SEGMENTS         (16)
 #define HAS_SD
 #define HAS_LCD
 #endif
 #if MACHINE_HARDWARE_VERSION == MAKELANGELO_3_3
-#define MAX_SEGMENTS         (16)  // has LCD, needs more ram.
+#define MAX_SEGMENTS         (16)
 #define USE_LIMIT_SWITCH
 #define HAS_SD
 #define HAS_LCD
 #endif
 #if MACHINE_HARDWARE_VERSION == MAKELANGELO_5
-#define MAX_SEGMENTS         (16)  // has LCD, needs more ram.
+#define MAX_SEGMENTS         (16)
 #define USE_LIMIT_SWITCH
 #define HAS_SD
 #define HAS_LCD
 #endif
 #if MACHINE_HARDWARE_VERSION == MAKELANGELO_6
-#define MAX_SEGMENTS         (16)  // has LCD, needs more ram.
+#define MAX_SEGMENTS         (16)
 #define USE_LIMIT_SWITCH
 #define HAS_TMC2130  // driver type
 #define STEALTHCHOP  // quiet operation
@@ -98,7 +99,7 @@
 #define CAN_HOME
 #endif
 
-extern void calibrateBelts();
+//extern void calibrateBelts();
 extern void recordHome();
 
 
