9cfdf136ea0039a7dc2f8b282cc45e8d48b56eec
Dan Royer <dan@marginallyclever.com>
2020-07-30 09:10:33 -0700

    Added basic SCARA IK

---
+++ robot_scara.h
@@ -0,0 +1,78 @@
+#pragma once
+//------------------------------------------------------------------------------
+// Makelangelo - firmware for various robot kinematic models
+// dan@marginallycelver.com 2013-12-26
+// Please see http://www.github.com/MarginallyClever/makelangeloFirmware for more information.
+//------------------------------------------------------------------------------
+
+#if MACHINE_STYLE == SCARA
+
+#define MACHINE_STYLE_NAME       "SCARA"
+#define MACHINE_HARDWARE_VERSION 1
+#define MACHINE_HAS_LIFTABLE_PEN
+
+// bicep is the connection between the base and the elbow.
+#define BICEP_LENGTH_MM      (150.0)
+// forearm is the connection between the elbow and the pen center
+#define FOREARM_LENGTH_MM    (110.0)
+
+
+#define STEP_DELAY           (50)  // delay between steps, in milliseconds, when doing fixed tasks like homing
+
+// servo angles for pen control
+#define PEN_UP_ANGLE         (90)
+#define PEN_DOWN_ANGLE       (50)  // Some steppers don't like 0 degrees
+
+#ifndef NUM_AXIES
+#define NUM_AXIES            (3)  // could be more?
+#endif
+
+#define NUM_MOTORS           (2)
+#define NUM_SERVOS           (1)
+#define NUM_TOOLS            (1)
+
+#define MAX_FEEDRATE         (100.0)  // depends on timer interrupt & hardware
+#define MIN_FEEDRATE         (0.0)
+#define DEFAULT_FEEDRATE     (90.0)
+
+#define MAX_ACCELERATION     (500.0)
+#define MIN_ACCELERATION     (0.0)
+#define DEFAULT_ACCELERATION (180.0)
+
+#define MAX_JERK             (10.0)
+#define MAX_Z_JERK           (0.3)
+
+// plan long moves as a set of submoves to increase accuracy.  Uncomment to turn this off.
+//#define SUBDIVIDE_LINES
+#define SEGMENTS_PER_SECOND (10)
+
+//------------------------------------------------------------------------------
+// MOTOR DETAILS
+//------------------------------------------------------------------------------
+
+// 400 step-per-turn motors move 0.9 degrees per step.  (360/400=0.9).  Marginallyclever.com default.
+// 200 step-per-turn motors move 1.8 degrees per step.  (360/200=1.8)
+// see your stepper motor data sheet for more info.
+#ifndef DEGREES_PER_STEP
+#define DEGREES_PER_STEP     (1.8)
+#endif
+
+// stepper motor drivers can use microstepping to split steps into fractions of steps for greater precision.
+// A4988 drivers (Marginallyclever.com default) use 16x.
+// DRV8825 can go up to 32x.
+// TMC2130 can go to 256x.
+// see your driver data sheet.
+// note that some boards have dip switches or jumpers that can be activated to turn stepping on and off.
+// make sure your dip switch settings match the firmware value.
+#ifndef MICROSTEPS
+#define MICROSTEPS           (16.0)
+#endif
+
+// These numbers are calculated from the above.  No need to change these.
+#ifndef NORMAL_MOTOR_STEPS
+#define NORMAL_MOTOR_STEPS   (360.0/DEGREES_PER_STEP)
+#endif
+#define STEPS_PER_TURN       (NORMAL_MOTOR_STEPS * MICROSTEPS)
+#define MICROSTEP_PER_DEGREE (STEPS_PER_TURN/360.0)
+
+#endif  // #ifdef TRADITIONALXY
