5551f6ce85a0484f29b6d9146996283cbcb138eb
Will Schaller <willschaller@gmail.com>
2020-12-06 22:33:14 +0100

    Remove irrelevant comment

--- board_cncv3.h
+++ board_cncv3.h
@@ -5,11 +5,7 @@
 // Please see http://www.github.com/MarginallyClever/makelangeloFirmware for more information.
 //------------------------------------------------------------------------------
 
-// https://www.instructables.com/id/Programming-the-WeMos-Using-Arduino-SoftwareIDE/
-
 // cnc shield pins: https://blog.protoneer.co.nz/arduino-cnc-shield/arduino-cnc-shield-scematics-v3-xx/
-// WEMOS D1 R2 pins: https://protosupplies.com/wp-content/uploads/2018/07/Wemos-D1-Pin-Differences.pdf.jpg
-// WEMOS D1 R2 board type should be set to "LOLIN(WEMOS) R1 D2 & mini"
 
 #if MOTHERBOARD == BOARD_CNCV3 
 
